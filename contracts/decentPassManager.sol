971f114818623dc52961723b41c80ab51fcf6c95
deluca-mike <michael.deluca@circle-free.com>
2019-10-31 04:15:19 -0400

    Incentivize Earlier Relaying

--- contracts/decentPassManager.sol
+++ contracts/decentPassManager.sol
@@ -7,10 +7,12 @@ contract DecentPassManager {
     event WithdrawalMade(address indexed account, uint256 indexed amount);
     event IndexIncremented(address indexed account, bytes32 saltKey);
     event EncryptedSeedSet(address indexed account);
+    event EarlyBonusFactorSet(address indexed account, uint256 indexed earlyBonusFactor);
 
     struct Account {
         uint256 balance;
         uint256 unlockBlock;
+        uint256 earlyBonusFactor;
         bytes32 encryptedSeed;
     }
 
@@ -32,15 +34,19 @@ contract DecentPassManager {
     }
 
     // get address of account that signed the arg with the proxy paramaters
-    function getAddressFromSig(bytes1 func, bytes32 arg, address relayer, uint256 gasprice, uint256 fee, uint256 nonce, uint256 expiry, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
-        return ecrecover(keccak256(abi.encodePacked(func, arg, relayer, gasprice, fee, nonce, expiry)), v, r, s);
+    function getAddressFromSig(bytes1 func, bytes32 arg, address relayer, uint256 gasprice, uint256 minFee, uint256 nonce, uint256 expiryBlock, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
+        return ecrecover(keccak256(abi.encodePacked(func, arg, relayer, gasprice, minFee, nonce, expiryBlock)), v, r, s);
     }
 
     // pay the relayer (the sender) as much as possible
-    function payRelayer(uint256 fee, Account storage account) internal {
-        uint256 possibleFee = account.balance >= fee ? fee : account.balance;
-        account.balance -= possibleFee;
-        accounts[msg.sender].balance += possibleFee;
+    function payRelayer(uint256 minFee, Account storage account, uint256 expiryBlock) internal {
+        // the fee will be approximately earlyBonusFactor tenths of a percent more for every block earlier the transaction is relayed
+        uint256 calculatedFee = minFee + (minFee >> 10) * (expiryBlock - block.number) * account.earlyBonusFactor;
+
+        // if the account doesn't have enough balance, just take it all
+        uint256 fee = account.balance >= calculatedFee ? calculatedFee : account.balance;
+        account.balance -= fee;
+        accounts[msg.sender].balance += fee;
     }
 
     // fallback to result in fund deposit
@@ -49,10 +55,11 @@ contract DecentPassManager {
     }
 
     // create an account by funding it, assigning a singer for relayed calls, and setting an encrypted seed
-    function create(address signingAddress, bytes32 encryptedSeed) public payable {
-        deposit();
-        addSigner(signingAddress);
-        setEncryptedSeed(encryptedSeed);
+    function create(address signingAddress, bytes32 encryptedSeed, uint256 earlyBonusFactor) public payable {
+        if (msg.value != 0) deposit();
+        if (signingAddress != address(0)) addSigner(signingAddress);
+        if (encryptedSeed != bytes32(0)) setEncryptedSeed(encryptedSeed);
+        if (earlyBonusFactor != uint256(0)) setEarlyBonusFactor(earlyBonusFactor);
     }
 
     // deposit ETH (in wei) into account, and lock withdrawing for 24 hours, which helps against front-running a relayer
@@ -80,10 +87,20 @@ contract DecentPassManager {
         emit SignerRemoved(msg.sender, signingAddress);
     }
 
+    // set an early bonus factor for relayed transactions
+    function setEarlyBonusFactor(uint256 earlyBonusFactor) public {
+        Account storage account = accounts[msg.sender];
+        assert(earlyBonusFactor > account.earlyBonusFactor || block.number >= account.unlockBlock);
+        account.earlyBonusFactor = earlyBonusFactor;
+        account.unlockBlock = block.number + 7200;
+
+        emit EarlyBonusFactorSet(msg.sender, earlyBonusFactor);
+    }
+
     // withdraw all ETH (in wei) in account, only if they've waited 24 hours since the last deposit
     function withdraw() public {
         Account storage account = accounts[msg.sender];
-        assert(block.number > account.unlockBlock);
+        assert(block.number >= account.unlockBlock);
 
         uint256 value = account.balance;
         account.balance = 0;
@@ -107,45 +124,45 @@ contract DecentPassManager {
     }
 
     // increment the index by proxy, where the relayer provides a signature that implies the account
-    function incrementIndexByProxy(bytes32 saltKey, uint256 fee, uint256 nonce, uint256 expiryBlock, uint8 v, bytes32 r, bytes32 s) public {
+    function incrementIndexByProxy(bytes32 saltKey, uint256 minFee, uint256 nonce, uint256 expiryBlock, uint8 v, bytes32 r, bytes32 s) public {
         // get signingAddress from signature of paramaters
-        address signingAddress = getAddressFromSig(0x01, saltKey, msg.sender, tx.gasprice, fee, nonce, expiryBlock, v, r, s);
+        address signingAddress = getAddressFromSig(0x01, saltKey, msg.sender, tx.gasprice, minFee, nonce, expiryBlock, v, r, s);
 
         // get account address from delegate mapping, then get reference to account itself
         address accountAddress = delegations[signingAddress];
         Account storage account = accounts[accountAddress];
 
         // assert signature is not expired, assert nonce is valid, and increment nonce (replay protection)
-        assert(block.number < expiryBlock);
+        assert(block.number <= expiryBlock);
         assert(nonce == nonces[getNonceKey(accountAddress, msg.sender)]++);
 
         // increment the index
         indices[getIndexKey(saltKey, accountAddress)]++;
 
         // refund the relayer as much as possible
-        payRelayer(fee, account);
+        payRelayer(minFee, account, expiryBlock);
 
         emit IndexIncremented(accountAddress, saltKey);
     }
 
     // set encrypted seed by proxy, where the relayer provides a signature that implies the account
-    function setEncryptedSeedByProxy(bytes32 encryptedSeed, uint256 fee, uint256 nonce, uint256 expiryBlock, uint8 v, bytes32 r, bytes32 s) public {
+    function setEncryptedSeedByProxy(bytes32 encryptedSeed, uint256 minFee, uint256 nonce, uint256 expiryBlock, uint8 v, bytes32 r, bytes32 s) public {
         // get signingAddress from signature of paramaters
-        address signingAddress = getAddressFromSig(0x02, encryptedSeed, msg.sender, tx.gasprice, fee, nonce, expiryBlock, v, r, s);
+        address signingAddress = getAddressFromSig(0x02, encryptedSeed, msg.sender, tx.gasprice, minFee, nonce, expiryBlock, v, r, s);
 
         // get account address from delegate mapping, then get reference to account itself
         address accountAddress = delegations[signingAddress];
         Account storage account = accounts[accountAddress];
 
         // assert signature is not expired, assert nonce is valid, and increment nonce (replay protection)
-        assert(block.number < expiryBlock);
+        assert(block.number <= expiryBlock);
         assert(nonce == nonces[getNonceKey(accountAddress, msg.sender)]++);
 
         // set the encrypted seed
         account.encryptedSeed = encryptedSeed;
 
         // refund the relayer as much as possible
-        payRelayer(fee, account);
+        payRelayer(minFee, account, expiryBlock);
 
         emit EncryptedSeedSet(accountAddress);
     }
@@ -155,9 +172,14 @@ contract DecentPassManager {
         return accounts[account].balance;
     }
 
-    // get an account's unlockBlock
+    // get the block at which an account is unlocked
     function getUnlockBlock(address account) public view returns (uint256) {
         return accounts[account].unlockBlock;
+    }
+
+    // get the early bonus factora a relayer will get for an account
+    function getEarlyBonusFactor(address account) public view returns (uint256) {
+        return accounts[account].earlyBonusFactor;
     }
 
     // get the raley nonce given adn account and relayer
